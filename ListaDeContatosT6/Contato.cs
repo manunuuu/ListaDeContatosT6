d56b7be67022e19d46a21c654c2269288a3d6577
manunuuu <manuelafortunato07@gmail.com>
2024-04-25 15:24:27 -0300

    criado o metodo Tostring()
    Tostring adicionado no metodo

--- ListaDeContatosT6/Contato.cs
+++ ListaDeContatosT6/Contato.cs
@@ -8,5 +8,73 @@ namespace ListaDeContatosT6
 {
     internal class Contato
     {
+        private string nome;
+        private string sobrenome;
+        private string telefone;
+
+        public string Nome
+        { 
+        get
+            {
+                return nome;
+            }
+
+        set { 
+            nome = value;
+            } 
+        }
+        public string Sobrenome
+        { 
+            get
+            { 
+                return sobrenome;
+            }
+            set
+            {
+                sobrenome = value;
+            }
+        }
+        public string Telefone
+        {
+            get
+            {
+                return telefone;
+            }
+            set
+            {
+                if (value.Length == 13)
+                {
+                   
+                    telefone = value;
+                }
+                else
+                {
+                    telefone = "(00) 00000-0000";
+                }
+            }
+        }
+         public Contato()
+        {
+            Nome = "claudio";
+            Sobrenome = "genésio 2";
+            Telefone = "(11) 98675-1143";
+            
+        }
+        public Contato(string nome, string sobrenome, string telefone)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Telefone = telefone;
+        }
+        public override string.ToString()
+        {
+            string saida = string.Empty;
+            saida += string.Format("{0} {1} ", Nome, Sobrenome);
+            saida += string.Format("{0} {1}-{2} ",
+                Telefone.Substring(0, 2),
+                Telefone.Substring(3, 5),
+                Telefone).Substring(7,4));
+            return saida;
+        }
     }
 }
